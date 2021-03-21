190db036a95690da60911c9f898f6641a0c76de8
Chris Harris <cjharris9908@gmail.com>
2021-03-21 15:11:01 -0600

    initial
    creating initial files for class

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# web-330
-Repository for Web 330 class
+<h1>WEB 330 Enterprise JavaScript II<h1>
+<h2>Contributors<h2>
+
+<li>Prof Krasso</li>
+<br>
+<li>Chris Harris</li>
---
+++ theme.js
@@ -0,0 +1,13 @@
+function setSelectedTheme(){
+    document.body.classList.value = localStorage.getItem("mode") || "light-theme"; 
+}
+
+function setDefaultTheme(){
+    const theme = localStorage.getItem("mode") || "light-theme";
+    const iconMode = localStorage.getItem("iconMode") || "fa-toggle-off";
+    const iconText = localStorage.getItem("iconText") || "Light Mode";
+
+    document.body.classList.value = theme;
+    document.getElementById("icon-mode").classList.add(iconMode);
+    document,getElementById("icon-text").innerHTML = iconText;
+}
