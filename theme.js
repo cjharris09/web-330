9339a7c2176f61a6344ce1aab124452f5dd10b82
Chris Harris <cjharris9908@gmail.com>
2021-03-21 15:40:28 -0600

    adding comments
    adding comments to this weeks files

--- theme.js
+++ theme.js
@@ -1,8 +1,18 @@
-function setSelectedTheme(){
+/*
+============================================
+; Title: javascript file for WEB330
+; Author: Chris Harris
+; Date: 21 March 2021
+; css file for the course
+;===========================================
+*/
+
+
+function setSelectedTheme(){//functions for setting the theme
     document.body.classList.value = localStorage.getItem("mode") || "light-theme"; 
 }
 
-function setDefaultTheme(){
+function setDefaultTheme(){//functions for setting the theme
     const theme = localStorage.getItem("mode") || "light-theme";
     const iconMode = localStorage.getItem("iconMode") || "fa-toggle-off";
     const iconText = localStorage.getItem("iconText") || "Light Mode";
