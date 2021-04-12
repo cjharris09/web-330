d57b4a8ca22f46a634745c2f4a205413e54d656a
Chris Harris <cjharris9908@gmail.com>
2021-04-11 19:23:14 -0600

    Week 4 work
    Adding the work for week 4

---
+++ week-4/calorie-converter.js
@@ -0,0 +1,26 @@
+/*
+============================================
+; Title: Assignment 4.2
+; Author: Chris Harris
+; Date: 11 April 2021
+; Description: Week 4 calorie app.
+;===========================================
+*/
+// imports the FoodModel function from the food-model.js file
+import { FoodModel } from './food-model.js';
+//Creates the calorie converted class that hold the array where the elements are held
+export class CalorieConverter { 
+    static data = [
+        new FoodModel (1007, 'Egg', 78),
+        new FoodModel (1008, 'Apple', 95),
+        new FoodModel (1009, 'Hamburger', 354),
+        new FoodModel (1010, 'Fries', 400),
+        new FoodModel (1011, 'Banana', 105),
+        new FoodModel (1012, 'Soda', 150),
+    ];
+
+    static find(searchString){
+        return this.data.filter(foodModel => foodModel.name.toLowerCase() == searchString.toLowerCase());
+    }
+    
+}
---
+++ week-4/food-model.js
@@ -0,0 +1,18 @@
+/*
+============================================
+; Title: Assignment 4.2
+; Author: Chris Harris
+; Date: 11 April 2021
+; Description: Week 4 calorie app.
+;===========================================
+*/
+
+// Creates the food model class and exports it
+export class FoodModel {
+    constructor(id, name, calories){
+        this.id = id;
+        this.name = name;
+        this.calories = calories;
+    }
+
+}
